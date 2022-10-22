9cd314e7a7bc0ee0c77f47495185ab225d4a8ee0
wngks8321 <wngks8321@gmail.com>
2022-10-22 15:37:55 +0900

    225 쪽 연습문제

--- Chapter7/src/array/StudentArray.java
+++ Chapter7/src/array/StudentArray.java
@@ -1,18 +1,32 @@
 package array;
 
+import java.util.ArrayList;
+
 public class StudentArray {
 
 	public static void main(String[] args) {
 		
 		Student[] studentArray = new Student[3];
+		ArrayList<Student> studentArray1 = new ArrayList<Student>();
 		
 		studentArray[0] = new Student(1001, "James");
 		studentArray[1] = new Student(1002, "Tomas");
 		studentArray[2] = new Student(1003, "Edward");
 		
+		studentArray1.add(new Student(1001, "James"));
+		studentArray1.add(new Student(1002, "Tomas"));
+		studentArray1.add(new Student(1003, "Edward"));
+		
 		for(Student s : studentArray) {
 			s.showStudentInfo();
 		}
+		
+		System.out.println("====================");
+		for(int i = 0; i < studentArray1.size(); i++) {
+			studentArray1.get(i).showStudentInfo();
+		}
+		
+		
 	}
 
 }
