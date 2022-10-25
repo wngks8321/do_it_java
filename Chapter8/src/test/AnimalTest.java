0c03a61cb4d54ee06ed96a093e48715c89a95184
wngks8321 <wngks8321@gmail.com>
2022-10-25 21:39:45 +0900

    다운캐스팅

--- Chapter8/src/test/AnimalTest.java
+++ Chapter8/src/test/AnimalTest.java
@@ -10,11 +10,19 @@ class Human extends Animal {
 	public void move() {
 		System.out.println("사람이 두발로 걷습니다.");
 	}
+	
+	public void readBook() {
+		System.out.println("사람이 책을 읽습니다.");
+	}
 }
 
 class Tiger extends Animal {
 	public void move() {
 		System.out.println("호랑이가 네발로 뜁니다.");
+	}
+	
+	public void hunting() {
+		System.out.println("호랑이가 사냥을 합니다.");
 	}
 }
 
@@ -22,6 +30,10 @@ class Eagle extends Animal {
 	public void move() {
 		System.out.println("독수리가 하늘을 날아다닙니다.");
 	}	
+	
+	public void flying() {
+		System.out.println("독수리가 날아다닙니다.");
+	}
 }
 
 public class AnimalTest {
@@ -32,13 +44,25 @@ public class AnimalTest {
 		test.moveAnimal(new Human()); //사람이 두발로 걷습니다.
 		test.moveAnimal(new Tiger()); // 호랑이가 네발로 뜁니다.
 		test.moveAnimal(new Eagle()); //독수리가 하늘을 날아다닙니다.
-		
-		
-		
 	}
 
 	public void moveAnimal(Animal animal) {
 		// 1. 아래 animal.move() 코드 한줄로 결과가 다양하게 나타남.
 		animal.move();
+		
+		// 다운캐스팅을 위해서 형태를 체크하는 조건문 
+		if(animal instanceof Human) {
+			// moveAnimal의 매개면수로 들어온 Animal타입의 animal을 Human타입으로 다운캐스팅해서 human 변수에 대입. 
+			Human human = (Human)animal;
+			human.readBook();
+		} else if(animal instanceof Tiger) {
+			Tiger tiger = (Tiger)animal;
+			tiger.hunting();
+		} else if(animal instanceof Eagle) {
+			Eagle eagle = (Eagle)animal;
+			eagle.flying();
+		} else {
+			System.out.println("지원되지 않는 기능입니다.");
+		}
 	}
 }
