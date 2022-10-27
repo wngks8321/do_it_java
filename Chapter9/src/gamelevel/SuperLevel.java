8d6db0f0bec2be71bafcf56dbcf9b4d4a8008f38
wngks8321 <wngks8321@gmail.com>
2022-10-27 22:12:43 +0900

    추상클래스와 템플릿 메서드 활용

---
+++ Chapter9/src/gamelevel/SuperLevel.java
@@ -0,0 +1,25 @@
+package gamelevel;
+
+public class SuperLevel extends PlayerLevel {
+
+	@Override
+	public void run() {
+		System.out.println("매우 빠르게 달립니다.");
+	}
+
+	@Override
+	public void jump() {
+		System.out.println("매우 높게 jump 합니다.");		
+	}
+
+	@Override
+	public void turn() {
+		System.out.println("한바퀴 돕니다.");		
+	}
+
+	@Override
+	public void showLevelMessage() {
+		System.out.println("*****고급자 레벨입니다.*****");		
+	}
+
+}
